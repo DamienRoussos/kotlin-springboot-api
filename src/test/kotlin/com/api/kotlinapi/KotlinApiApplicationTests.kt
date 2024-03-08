6d1d4eafd40b0ba7540cd4f58fd22485e34c3796
Damien Roussos <damienroussos@gmail.com>
2024-03-08 11:57:18 +0100

    error with testing, specified the spring boot application in the test file

--- src/test/kotlin/com/api/kotlinapi/KotlinApiApplicationTests.kt
+++ src/test/kotlin/com/api/kotlinapi/KotlinApiApplicationTests.kt
@@ -1,9 +1,10 @@
 package com.api.kotlinapi
 
+import com.api.kotlinapi.application.KotlinApiApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [KotlinApiApplication::class])
 class KotlinApiApplicationTests {
 
 	@Test
